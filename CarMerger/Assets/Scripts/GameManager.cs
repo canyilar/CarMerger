89ce18eb4d57821e4e0bfff78bec88f9a833b7c9
cenullum <57962685+cenullum@users.noreply.github.com>
2022-12-07 22:05:51 +0300

    Moving cars over road

---
+++ CarMerger/Assets/Scripts/GameManager.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using CarMerger;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _roadManager;
+    private List<GameObject > _carsOnRoad = new List<GameObject>();// car,target
+
+    private Transform _roadPoints;
+    private float _speed = 5;
+
+    public static GameManager Instance { get; private set; }    
+
+
+    public event Action<Car> OnCarLap;
+
+
+    private void Start()
+    {
+        Instance = this;
+        _roadPoints = _roadManager.transform.Find("RoadMap").Find("RoadPoints");
+    }
+
+    private void Update()
+    {
+        MoveCarsOnRoad();
+    }
+
+
+    private void MoveCarsOnRoad()
+    {
+        List<GameObject> moveNextRoadPoint = new List<GameObject>();
+        foreach (GameObject carObject in _carsOnRoad)
+        {
+            Car car = carObject.GetComponent<Car>();
+            Transform roadPointTarget = car.TargetRoadPoint;
+
+            if (IsVector3Same(roadPointTarget.position, carObject.transform.position)) 
+            {
+                //if car is on roadpoint set next target
+                // moveNextRoadPoint.Add(car);
+               
+                Transform nextRoadPoint = GetNextCheckPoint(roadPointTarget);
+                car.TargetRoadPoint = nextRoadPoint;
+                
+                if (nextRoadPoint.name == "Point")
+                {
+                    OnCarLap?.Invoke(car);
+                }
+                continue;
+            }
+
+            float distance = Vector3.Distance(car.transform.position, roadPointTarget.position);
+            float duration = distance / _speed;
+
+            carObject.transform.DOMove(roadPointTarget.position, duration);
+        }
+    }
+
+    public void SetCarToRoad(GameObject carObject)
+    {
+        Transform startPoint = _roadPoints.GetChild(0);
+        Transform nextPoint = _roadPoints.GetChild(1);
+        carObject.transform.position = startPoint.position;
+        Car car = carObject.GetComponent<Car>();
+        car.GetComponent<Collider>().enabled=false;
+        car.TargetRoadPoint = nextPoint;
+        _carsOnRoad.Add(carObject);
+    }
+
+    public void RemoveCarFromRoad(GameObject car)
+    {
+        _carsOnRoad.Remove(car);
+    }
+
+    private Transform GetNextCheckPoint(Transform currentCheckpoint)
+    {
+        int childrenCount = _roadPoints.childCount;
+        for (int i = 0; i < childrenCount; i++)
+        {
+            if (_roadPoints.GetChild(i)== currentCheckpoint)
+            {
+                if (i== childrenCount-1)
+                {
+                   
+                    return _roadPoints.GetChild(0);
+                    
+                }
+                else
+                {
+                   
+                    return _roadPoints.GetChild(i+1);
+                }
+
+            }
+        }
+
+        return null;
+        
+    }
+
+    private bool IsVector3Same(Vector3 a, Vector3 b)
+    {
+        if (Mathf.Approximately(a.x, b.x)==false)
+        {
+            return false;
+        }
+        if (Mathf.Approximately(a.y, b.y) == false)
+        {
+            return false;
+        }
+        if (Mathf.Approximately(a.z, b.z) == false)
+        {
+            return false;
+        }
+        return true;
+    }
+
+
+
+}
