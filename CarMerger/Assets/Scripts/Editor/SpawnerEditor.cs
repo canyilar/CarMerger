d4d429f0731a88eb725bd8685d6879368a3a207b
cenullum <57962685+cenullum@users.noreply.github.com>
2022-12-12 21:42:22 +0300

    cars

--- CarMerger/Assets/Scripts/Editor/SpawnerEditor.cs
+++ CarMerger/Assets/Scripts/Editor/SpawnerEditor.cs
@@ -3,39 +3,39 @@ using UnityEngine;
 
 namespace CarMerger.Editors
 {
-    [CustomEditor(typeof(Spawner))]
+    //[CustomEditor(typeof(Spawner))]
     public class SpawnerEditor : Editor
     {
-        public override void OnInspectorGUI()
-        {
-            Spawner carSpawner = (Spawner)target;
-            SerializedObject obj = new(carSpawner);
+        //public override void OnInspectorGUI()
+        //{
+        //    Spawner carSpawner = (Spawner)target;
+        //    SerializedObject obj = new(carSpawner);
 
-            SerializedProperty carPrefab = obj.FindProperty("_carPrefabs");
-            SerializedProperty carHolder = obj.FindProperty("_carHolder");
-            SerializedProperty spawnOnStart = obj.FindProperty("_spawnCarsOnStart");
+        //    SerializedProperty carPrefab = obj.FindProperty("_carPrefabs");
+        //    SerializedProperty carHolder = obj.FindProperty("_carHolder");
+        //    SerializedProperty spawnOnStart = obj.FindProperty("_spawnCarsOnStart");
 
-            EditorGUILayout.PropertyField(carPrefab);
-            EditorGUILayout.PropertyField(spawnOnStart);
-            EditorGUILayout.PropertyField(carHolder);
+        //    EditorGUILayout.PropertyField(carPrefab);
+        //    EditorGUILayout.PropertyField(spawnOnStart);
+        //    EditorGUILayout.PropertyField(carHolder);
 
-            GUILayout.Space(10);
+        //    GUILayout.Space(10);
 
-            if (GUILayout.Button("Spawn Car"))
-            {
-                carSpawner.SpawnCar();
-            }
+        //    if (GUILayout.Button("Spawn Car"))
+        //    {
+        //        carSpawner.SpawnCar();
+        //    }
 
-            string[] exlucededProps = new string[]
-            {
-                "m_Script",
-                carHolder.name,
-                carPrefab.name,
-                spawnOnStart.name,
-            };
-            DrawPropertiesExcluding(obj, exlucededProps);
-            obj.ApplyModifiedProperties();
-        }
+        //    string[] exlucededProps = new string[]
+        //    {
+        //        "m_Script",
+        //        carHolder.name,
+        //        carPrefab.name,
+        //        spawnOnStart.name,
+        //    };
+        //    DrawPropertiesExcluding(obj, exlucededProps);
+        //    obj.ApplyModifiedProperties();
+        //}
 
     }
 }
